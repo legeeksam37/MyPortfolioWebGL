1bc1cb273abd8accb6a3238615044ed396dfb99f
legeeksam37 <65701874+legeeksam37@users.noreply.github.com>
2024-11-24 22:22:00 +0100

    init - webGL

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-Doppelganger-0.1";
+const contentToCache = [
+    "Build/BuildsWebGL.loader.js",
+    "Build/BuildsWebGL.framework.js",
+    "Build/BuildsWebGL.data",
+    "Build/BuildsWebGL.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
